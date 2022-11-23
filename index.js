690c4fafdd19c4300557835df6a16f3cbf42bc74
prateek11113edd <prateek123444@gmail.com>
2022-11-23 15:02:17 -0600

    added cors, express json parser and create connection to mongodb

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 
 const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.URI, {
+  useNewUrlParser: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
